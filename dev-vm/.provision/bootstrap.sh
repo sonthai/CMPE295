0a10ceac12aeb83a60535d50492c2bcf9dd220bc
Son Thai <thaison242@gmail.com>
2018-03-21 00:51:09 -0700

    Fix vagrant deployment

--- dev-vm/.provision/bootstrap.sh
+++ dev-vm/.provision/bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "************* Start Setting up ***********"
+echo "Root $ROOT"
+echo "******************************************"
 
 echo "Installing maven, git, curl, and java8 ..."
 sudo apt-get -y install maven git curl
@@ -6,14 +9,19 @@ sudo apt-get install -y default-jdk
 
 echo "Installing python and package dependency ..."
 python3 -V
-sudo apt-get upgrade
+sudo apt-get -y update
+#sudo apt-get install cuda-9.0
 sudo apt-get install -y python3-pip python3-dev
 pip3 install tensorflow
-pip3 install tensorflow-gpu
+#pip3 install tensorflow-gpu
 pip3 install psutil
+pip3 install annoy 
+pip3 install scipy 
+pip3 install nltk
+#pip3 install --upgrade tensorflow
 
 echo "Installing tomcat8..."
-sudo echo "export CATALINA_OPTS=\"-Xms512m -Xmx1024m\"" >> /home/vagrant/.bashrc
+sudo echo "export CATALINA_OPTS=\"-Xms512m -Xmx1024m\"" >> $ROOT/.bashrc
 sudo apt-get -y install tomcat8 tomcat8-docs tomcat8-examples tomcat8-admin
 
 echo "Installing Apache Cassandra..."
@@ -35,7 +43,7 @@ sudo mkdir /opt/kafka
 sudo tar -xvf kafka_2.11-1.0.0.tgz -C /opt/kafka
 sudo rm kafka_2.11-1.0.0.tgz
 sudo nohup /opt/kafka/kafka_2.11-1.0.0/bin/kafka-server-start.sh /opt/kafka/kafka_2.11-1.0.0/config/server.properties > /tmp/kafka.log 2>&1 &
-echo "Waiting 10s for kakfta to start..."
+echo "Waiting 10s for kafka to start..."
 sleep 10
 
 echo "Testing Kafka server..."
@@ -44,19 +52,23 @@ echo "Listing available topic on apache kafka..."
 sudo /opt/kafka/kafka_2.11-1.0.0/bin/kafka-topics.sh --list --zookeeper localhost:2181
 
 echo "Cleaning up..."
-sudo rm -rf /home/vagrant/app; /home/vagrant/scripts
+sudo rm -rf $ROOT/app; $ROOT/engine
 
 echo "Building and deploying app-1"
-sudo cp -R /mnt/app /home/vagrant/app
-cd /home/vagrant/app; mvn clean install
+sudo cp -R /mnt/app $ROOT/app
+cd $ROOT/app; mvn clean install
 sudo cp target/app-1.war /var/lib/tomcat8/webapps/
 echo "Restarting tomcat server"
 sudo service tomcat8 restart
 
-echo "Copying classify image scripts to scripts folder ..."
+echo "Copying engine folder ..."
 cd /mnt
-sudo cp -R scripts/classify_images.py /home/vagrant/
+sudo cp -R engine/ $ROOT
 
-echo "Create uploads folder to store uploaded image ..."
-mkdir /home/vagrant/uploads
-sudo chown tomcat8:tomcat8 /home/vagrant/uploads
+echo "Create uploads folder to store uploaded images ..."
+mkdir -p $ROOT/engine/uploads
+sudo chown tomcat8:tomcat8 $ROOT/engine/uploads
+
+echo "Testing tensorflow set up"
+sudo cp -R tests/ $ROOT
+python3 tests/tftest.py
