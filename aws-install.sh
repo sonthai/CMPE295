5ceb64c1c5aecc17c645fd7981831b0d1607852f
Ubuntu <ubuntu@ip-172-31-30-20.us-west-2.compute.internal>
2018-03-24 08:49:58 +0000

    Modify aws-install.sh script

--- aws-install.sh
+++ aws-install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "************* Start Setting up ***********"
-export ROOT="/home/ubuntu/CMPE295"
+export ROOT="/home/ubuntu"
+export CMPE295="$ROOT/CMPE295"
 echo "Root $ROOT"
 echo "******************************************"
 
@@ -31,38 +32,40 @@ sudo apt-get install -y cassandra
 echo "Starting Apache Cassandra..."
 sudo sudo service cassandra start
 
-echo "Install Zookepper..."
-sudo apt-get install -y zookeeperd
-netstat -ant | grep :2181
-
-echo "Installing and Starting Kafka Server..."
+echo "Downloading kafka jar file ...."
 curl -O http://mirrors.advancedhosters.com/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz
-sudo mkdir /opt/kafka
-sudo tar -xvf kafka_2.11-1.0.0.tgz -C /opt/kafka
-sudo rm kafka_2.11-1.0.0.tgz
-sudo echo "export KAFKA_HEAP_OPTS=\"-Xms500m -Xmx500m\"" >> $ROOT/../.bashrc
-source $ROOT/../.bashrc
-sudo nohup /opt/kafka/kafka_2.11-1.0.0/bin/kafka-server-start.sh /opt/kafka/kafka_2.11-1.0.0/config/server.properties > /tmp/kafka.log 2>&1 &
+mkdir $ROOT/kafka
+tar -xvf kafka_2.11-1.0.0.tgz -C $ROOT/kafka
+rm kafka_2.11-1.0.0.tgz
+echo "export KAFKA_HEAP_OPTS=\"-Xms500m -Xmx500m\"" >> $ROOT/.bashrc
+source $ROOT/.bashrc
+echo "Starting zookeeper server..."
+nohup $ROOT/kafka/kafka_2.11-1.0.0/bin/zookeeper-server-start.sh $ROOT/kafka/kafka_2.11-1.0.0/config/zookeeper.properties > $ROOT/kafka/zookeeper.log 2>&1 &
+echo "Waiting 10s for zookeeper to start..."
+sleep 10
+
+netstat -ant | grep :2181
+echo "Starting kafka server..."
+nohup $ROOT/kafka/kafka_2.11-1.0.0/bin/kafka-server-start.sh $ROOT/kafka/kafka_2.11-1.0.0/config/server.properties > $ROOT/kafka/kafka.log 2>&1 &
 echo "Waiting 10s for kafka to start..."
 sleep 10
 
 echo "Testing Kafka server..."
-sudo /opt/kafka/kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 1 --topic testing
+sudo $ROOT/kafka/kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 1 --topic testing
 echo "Listing available topic on apache kafka..."
-sudo /opt/kafka/kafka_2.11-1.0.0/bin/kafka-topics.sh --list --zookeeper localhost:2181
+sudo $ROOT/kafka/kafka_2.11-1.0.0/bin/kafka-topics.sh --list --zookeeper localhost:2181
 
 
 echo "Building and deploying app-1"
-cd $ROOT/app; mvn clean install
+cd $CMPE295/app; mvn clean install
 sudo cp target/app-1.war /var/lib/tomcat8/webapps/
 echo "Restarting tomcat server"
 sudo service tomcat8 restart
 
 
 echo "Create uploads folder to store uploaded images ..."
-mkdir -p $ROOT/engine/uploads
-sudo chown tomcat8:tomcat8 $ROOT/engine/uploads
+mkdir -p $CMPE295/engine/uploads
+sudo chown tomcat8:tomcat8 $CMPE295/engine/uploads
 
-#echo "Testing tensorflow set up"
-#sudo cp -R tests/ $ROOT
-#python3 tests/tftest.py
+echo "Testing tensorflow set up"
+python3 $CMPE295/tests/tftest.py
