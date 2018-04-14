cc2ae9ed619d7eba62590b85e0ac867429800297
Andrew Wong <andrewwong@Andrewteki-MacBook-Pro.local>
2018-04-14 16:14:57 -0700

    no head assumption crop

--- jetson tx2/sender/image_transfer.py
+++ jetson tx2/sender/image_transfer.py
@@ -5,24 +5,35 @@ import time
 import urllib2
 import json
 import os
+from PIL import Image
 
-path = ‘images’
-url = ‘http://35.165.214.31:8080/app-1/customer/processData’
+path = 'images'
+url = 'http://35.165.214.31:8080/app-1/customer/processData'
 listOfFiles = [f for f in listdir(path) if isfile(join(path, f))]
-print listOfFiles
-
 for file in listOfFiles:
-   path_file = path+“/”+file
-   with open(path_file, “rb”) as image_file:
+   path_file = path+'/'+file
+   im = Image.open(path_file)
+   width, height = im.size
+   # print width, height
+   # im.show()
+   # cropping_90_size = ( width * .1, height * .1,  width - width * .1, height - height * .1)
+   cropping_no_top_size = ( 0, height * .2,  width, height)
+   cropping_size = cropping_no_top_size
+   # cropping_size = cropping_90_size
+   # print cropping_size
+   cropped_im = im.crop(cropping_size)
+   # cropped_im.show()
+   cropped_im.save(path_file)
+   with open(path_file, "rb") as image_file:
        encoded_string = base64.b64encode(image_file.read())
        ts = int(time.time())
        payload = {}
-       payload[‘id’] = ts
-       payload[‘image’] = encoded_string
-       headers = {‘Content-type’: ‘application/json’}
+       payload['id'] = ts
+       payload['image'] = encoded_string
+       headers = {'Content-type': 'application/json'}
        data_json = json.dumps(payload)
        request = urllib2.Request(url)
-       request.add_header(‘Content-Type’, ‘application/json’)
+       request.add_header('Content-Type', 'application/json')
        response = urllib2.urlopen(request, data_json)
-       print response
+       print path_file+" was sent!"
        os.remove(path_file)
