f4d27b4973a891a4660b0eacd2a9b9063246cad3
Son Thai <thaison242@gmail.com>
2018-04-22 01:24:38 -0700

    Support CORS

--- app/src/main/java/com/config/AppConfig.java
+++ app/src/main/java/com/config/AppConfig.java
@@ -3,9 +3,11 @@ package com.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@CrossOrigin(origins = "http://recommedation-ui.s3-website-us-west-2.amazonaws.com", maxAge = 3600)
 @ComponentScan("com.api")
 @EnableWebMvc
 @PropertySource("classpath:/application.properties")
